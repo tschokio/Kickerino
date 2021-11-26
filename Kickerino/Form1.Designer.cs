c26aa79fd96a64e2d13569f0aba4fa9d33488c92
tschokio <tschokio@hotmail.com>
2021-11-26 11:53:54 +0100

    removed unnecessary buttons

--- Kickerino/Form1.Designer.cs
+++ Kickerino/Form1.Designer.cs
@@ -47,20 +47,18 @@ namespace Kickerino
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // managePlayersButton
             // 
-            this.managePlayersButton.Location = new System.Drawing.Point(408, 430);
+            this.managePlayersButton.Location = new System.Drawing.Point(756, 738);
+            this.managePlayersButton.Margin = new System.Windows.Forms.Padding(6);
             this.managePlayersButton.Name = "managePlayersButton";
-            this.managePlayersButton.Size = new System.Drawing.Size(137, 23);
+            this.managePlayersButton.Size = new System.Drawing.Size(254, 49);
             this.managePlayersButton.TabIndex = 6;
             this.managePlayersButton.Text = "ManagePlayers";
             this.managePlayersButton.UseVisualStyleBackColor = true;
@@ -69,11 +67,12 @@ namespace Kickerino
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(35, 64);
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(65, 137);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6);
             this.listBox1.Name = "listBox1";
             this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.listBox1.Size = new System.Drawing.Size(137, 229);
+            this.listBox1.Size = new System.Drawing.Size(251, 484);
             this.listBox1.TabIndex = 7;
             this.listBox1.Format += new System.Windows.Forms.ListControlConvertEventHandler(this.listBox1_Format);
             this.listBox1.SizeChanged += new System.EventHandler(this.listBox1_SizeChanged);
@@ -81,19 +80,21 @@ namespace Kickerino
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 15;
-            this.listBox2.Location = new System.Drawing.Point(237, 64);
+            this.listBox2.ItemHeight = 32;
+            this.listBox2.Location = new System.Drawing.Point(440, 137);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(6);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(137, 229);
+            this.listBox2.Size = new System.Drawing.Size(251, 484);
             this.listBox2.TabIndex = 8;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             this.listBox2.Format += new System.Windows.Forms.ListControlConvertEventHandler(this.listBox2_Format);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(65, 430);
+            this.button2.Location = new System.Drawing.Point(849, 639);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(139, 49);
             this.button2.TabIndex = 9;
             this.button2.Text = "refresh";
             this.button2.UseVisualStyleBackColor = true;
@@ -101,11 +102,13 @@ namespace Kickerino
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(557, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(11, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(1034, 44);
             this.menuStrip1.TabIndex = 10;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -118,58 +121,60 @@ namespace Kickerino
             this.exitToolStripMenuItem,
             this.openToolStripMenuItem1});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(71, 36);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(206, 44);
             this.newToolStripMenuItem.Text = "New";
             this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(206, 44);
             this.saveToolStripMenuItem.Text = "Open";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(206, 44);
             this.openToolStripMenuItem.Text = "Save";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(206, 44);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem1
             // 
             this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
-            this.openToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(206, 44);
             this.openToolStripMenuItem1.Text = "Open";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(79, 46);
+            this.label1.Location = new System.Drawing.Point(147, 98);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.Size = new System.Drawing.Size(122, 32);
             this.label1.TabIndex = 11;
             this.label1.Text = "All Players";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(35, 346);
+            this.button1.Location = new System.Drawing.Point(65, 738);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.Size = new System.Drawing.Size(254, 49);
             this.button1.TabIndex = 12;
             this.button1.Text = "AutoAssign Jersey Number";
             this.button1.UseVisualStyleBackColor = true;
@@ -177,16 +182,18 @@ namespace Kickerino
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(237, 309);
+            this.textBox1.Location = new System.Drawing.Point(440, 659);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(45, 23);
+            this.textBox1.Size = new System.Drawing.Size(80, 39);
             this.textBox1.TabIndex = 13;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(305, 309);
+            this.button3.Location = new System.Drawing.Point(566, 659);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(69, 23);
+            this.button3.Size = new System.Drawing.Size(128, 49);
             this.button3.TabIndex = 14;
             this.button3.Text = "update #";
             this.button3.UseVisualStyleBackColor = true;
@@ -195,9 +202,10 @@ namespace Kickerino
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(87, 309);
+            this.comboBox1.Location = new System.Drawing.Point(162, 659);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(6);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(37, 23);
+            this.comboBox1.Size = new System.Drawing.Size(65, 40);
             this.comboBox1.TabIndex = 15;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
@@ -205,9 +213,10 @@ namespace Kickerino
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(132, 309);
+            this.comboBox2.Location = new System.Drawing.Point(245, 659);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(6);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(37, 23);
+            this.comboBox2.Size = new System.Drawing.Size(65, 40);
             this.comboBox2.TabIndex = 16;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             this.comboBox2.Click += new System.EventHandler(this.comboBox2_Click_1);
@@ -215,28 +224,20 @@ namespace Kickerino
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 317);
+            this.label2.Location = new System.Drawing.Point(22, 676);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.Size = new System.Drawing.Size(146, 32);
             this.label2.TabIndex = 17;
             this.label2.Text = "JerseyRange";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(237, 430);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 18;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(237, 346);
+            this.button5.Location = new System.Drawing.Point(440, 738);
+            this.button5.Margin = new System.Windows.Forms.Padding(6);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(137, 23);
+            this.button5.Size = new System.Drawing.Size(254, 49);
             this.button5.TabIndex = 19;
             this.button5.Text = "Save Jersey Numbers";
             this.button5.UseVisualStyleBackColor = true;
@@ -245,52 +246,31 @@ namespace Kickerino
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(261, 46);
+            this.label3.Location = new System.Drawing.Point(485, 98);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 15);
+            this.label3.Size = new System.Drawing.Size(186, 32);
             this.label3.TabIndex = 20;
             this.label3.Text = "Selected Players";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(289, 312);
+            this.label4.Location = new System.Drawing.Point(537, 666);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(12, 15);
+            this.label4.Size = new System.Drawing.Size(24, 32);
             this.label4.TabIndex = 21;
             this.label4.Text = "-";
             // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(190, 391);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 22;
-            this.button6.Text = "add squad memeber";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(12, 391);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(148, 23);
-            this.button7.TabIndex = 23;
-            this.button7.Text = "add selected to squad";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(557, 465);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
+            this.ClientSize = new System.Drawing.Size(1034, 821);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
@@ -304,6 +284,7 @@ namespace Kickerino
             this.Controls.Add(this.managePlayersButton);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Jersey Pick";
             this.menuStrip1.ResumeLayout(false);
@@ -330,12 +311,9 @@ namespace Kickerino
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
         private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
     }
